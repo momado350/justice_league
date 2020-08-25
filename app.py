7007025c0d2e2152a11cf2637e0c1ec01e6ba3a2
Mohammed Ahmed <mohammedabdalla38@yahoo.com>
2020-08-25 17:26:25 -0500

    our dict

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask, jsonify
+
+# Dictionary of Justice League
+justice_league_members = [
+    {"superhero": "Aquaman", "real_name": "Arthur Curry"},
+    {"superhero": "Batman", "real_name": "Bruce Wayne"},
+    {"superhero": "Cyborg", "real_name": "Victor Stone"},
+    {"superhero": "Flash", "real_name": "Barry Allen"},
+    {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
+    {"superhero": "Superman", "real_name": "Clark Kent/Kal-El"},
+    {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
+]
