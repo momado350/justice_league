066da45763ff44d720c95f2f58268fab7800547a
Mohammed Ahmed <mohammedabdalla38@yahoo.com>
2020-08-25 17:28:32 -0500

    create routes

--- app.py
+++ app.py
@@ -16,4 +16,28 @@ justice_league_members = [
 #################################################
 app = Flask(__name__)
 
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/justice-league")
+def justice_league():
+    """Return the justice league data as json"""
+
+    return jsonify(justice_league_members)
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Justice League API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/justice-league"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
